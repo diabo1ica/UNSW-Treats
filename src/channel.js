81c6e8d5de2f67b671285970cdaf6a13416a790a
Kenneth Kuo <k.kuo@student.unsw.edu.au>
2022-06-07 18:08:35 +1000

    Update channel.js

--- src/channel.js
+++ src/channel.js
@@ -6,8 +6,8 @@ function channelInviteV1(authUserId, channelId, uId) {
   return 'authUserId' + 'channelId' + 'uId';
 }
 
-function channelMessagesV1 (authUserId, ChannelId, start) {
-    return 'authUserId' + 'ChannelId' + 'start';
+function channelMessagesV1 (authUserId, channelId, start) {
+    return 'authUserId' + 'channelId' + 'start';
 }
 
 
