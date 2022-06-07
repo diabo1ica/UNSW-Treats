c0876effeebcba19eed1d3cd28b384bf0325e77b
David <z5361398@unsw.edu.au>
2022-06-07 16:50:06 +1000

    Adding auth login

--- src/channel.js
+++ src/channel.js
@@ -1,3 +1,7 @@
 function channelDetailsV1(authUserId, channelId){
   return 'authUserId' + 'channelId';
 }
+
+function channelJoinV1(authUserId, channelId) {
+  return 'authUserId' + 'channelId';
+}
