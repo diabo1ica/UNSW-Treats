d5ac02cfbe2d28ee46b92f32343d815aca8dec01
David <z5361398@unsw.edu.au>
2022-06-09 17:37:38 +1000

    merge conflicts

--- src/channel.js
+++ src/channel.js
@@ -2,10 +2,6 @@ function channelDetailsV1(authUserId, channelId){
   return 'authUserId' + 'channelId';
 }
 
-function channelJoinV1(authUserId, channelId) {
-  return 'authUserId' + 'channelId';
-}
-
 function channelInviteV1(authUserId, channelId, uId) {
   return 'authUserId' + 'channelId' + 'uId';
 }
@@ -14,5 +10,8 @@ function channelMessagesV1 (authUserId, channelId, start) {
     return 'authUserId' + 'channelId' + 'start';
 }
 
+function channelJoinsV1 (authUserId, channelId) {
+  return 'authUserId' + 'channelId';
+}
 
->>>>>>> 96cd6f43579ee894da1cf63a3bba5aae76c9b0cc
+
