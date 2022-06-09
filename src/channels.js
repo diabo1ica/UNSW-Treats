c24d2884ac615a683c5f5a1dbf976dc4f4a4df06
David Pei <david.pei@student.unsw.edu.au>
2022-06-09 18:24:04 +1000

    solve conflict

--- src/channels.js
+++ src/channels.js
@@ -1,3 +1,12 @@
+function channelsCreateV1(authUserId, name, isPublic) {
+  return 'authUserId' + 'name' + 'isPublic';
+}
+
+function channelsListallV1 (authUserId) {
+    return 'authUserId';
+}
+
 function channelsListV1 (authUserId) {
   return 'authUserId';
 }
+
