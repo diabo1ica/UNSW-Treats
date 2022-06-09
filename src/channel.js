143c66763c0edb2ab3dee5823e79e514a4590cd7
David Pei <david.pei@student.unsw.edu.au>
2022-06-09 18:21:54 +1000

    solve conflict

--- src/channel.js
+++ src/channel.js
@@ -2,6 +2,14 @@ function channelDetailsV1(authUserId, channelId){
   return 'authUserId' + 'channelId';
 }
 
+function channelInviteV1(authUserId, channelId, uId) {
+  return 'authUserId' + 'channelId' + 'uId';
+}
+
+function channelMessagesV1 (authUserId, channelId, start) {
+  return 'authUserId' + 'channelId' + 'start';
+}
+
 function channelJoinV1(authUserId, channelId) {
   return 'authUserId' + 'channelId';
 }
