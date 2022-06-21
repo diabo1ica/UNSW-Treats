d86665092ef6e864432830648ecd5735476b878e
Kenneth <kennethkuo25@outlook.com>
2022-06-21 15:57:26 +1000

    Added 4 ChannelInvite Tests

---
+++ src/channel.test.js
@@ -0,0 +1,83 @@
+import { channelInviteV1, channelJoinV1 } from './channel'
+import { authRegisterV1, authloginV1 } from './auth'
+import { channelsCreateV1 } from './channels'
+
+test ('Testing Invitation(exist)', () => {
+  const a = authRegisterV1('garyang@gmail.com', '12345678', 'Gary', 'Ang');
+  const u_id = a.authUserId;
+  
+  authRegisterV1('kennethkuo@gmail.com', '87654321', 'Kenneth', 'Kuo');
+  // the authUserId is Kenneth id, Kenneth is the author.
+  const b = authloginV1('kennethkuo@gmail.com', '87654321');
+  const user_authUserId = b.authUserId;
+  
+  // create Channel1 which isPublic
+  const c = channelsCreateV1(user_authUserId, 'Channel1', 'true');  
+  const channel_id = c.channelId;  
+  
+  expect(channelInviteV1(user_authUserId, channel_id, u_id)).toStrictEqual({});
+});
+
+
+test('Testing Invitation(do not exist (1))', () => {
+  const a = authRegisterV1('garyang@gmail.com', '12345678', 'Gary', 'Ang');
+  const u_id = a.authUserId;
+  
+  authRegisterV1('kennethkuo@gmail.com', '87654321', 'Kenneth', 'Kuo');
+  // the authUserId is Kenneth id, Kenneth is the author.
+  const b = authloginV1('kennethkuo@gmail.com', '87654321');
+  const user_authUserId = b.authUserId;
+  
+   // create Channel1 which isPublic
+  const c = channelsCreateV1(user_authUserId, 'Channel1', 'true');  
+  const channel_id = c.channelId;
+  
+  expect(channelInviteV1(user_authUserId, '10000000', u_id)).toStrictEqual({ error: 'error' });
+});
+
+
+test('Testing Invitation(do not exist (2))', () => {
+  const a = authRegisterV1('garyang@gmail.com', '12345678', 'Gary', 'Ang');
+  const u_id = a.authUserId;
+  
+  authRegisterV1('kennethkuo@gmail.com', '87654321', 'Kenneth', 'Kuo');
+  // the authUserId is Kenneth id, Kenneth is the author.
+  const b = authloginV1('kennethkuo@gmail.com', '87654321');
+  const user_authUserId = b.authUserId;
+  
+  // create Channel1 which isPublic
+  const c = channelsCreateV1(user_authUserId, 'Channel1', 'true');  
+  const channel_id = c.channelId;
+  
+  expect(channelInviteV1(user_authUserId, channel_id, 1000000)).toStrictEqual({ error: 'error' });
+});
+
+test('Testing Invitation(do not exist (3))', () => {
+  const a = authRegisterV1('garyang@gmail.com', '12345678', 'Gary', 'Ang');
+  const u_id = a.authUserId;
+  
+  authRegisterV1('kennethkuo@gmail.com', '87654321', 'Kenneth', 'Kuo');
+  // the authUserId is Kenneth id, Kenneth is the author.
+  const b = authloginV1('kennethkuo@gmail.com', '87654321');
+  const user_authUserId = b.authUserId;
+  
+  // create Channel1 which isPublic
+  const c = channelsCreateV1(user_authUserId, 'Channel1', 'true');  
+  const channel_id = c.channelId;
+  
+  expect(channelInviteV1(u_id, channel_id, user_authUserId)).toStrictEqual({ error: 'error' });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
