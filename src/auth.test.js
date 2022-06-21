a465c37c4185cbed972fa1b0816ed89c7eefb480
Steve <z5363495@unsw.edu.au>
2022-06-22 00:58:58 +1000

    Created test suite for authLoginV1

--- src/auth.test.js
+++ src/auth.test.js
@@ -38,3 +38,36 @@ describe('Sample test', () => {
       expect(authRegisterV1('validemail@gmail.com', 'drowssap', 'Strongjaw', last)).toStrictEqual({error: 'error'});
   });
 });
+
+describe('Test suite for authLoginV1', () => {
+  let user_id1;
+  let user_id2;
+  let user_id3;
+  let user_id4;
+  
+  beforeEach(() => {
+    user_id1 = authRegisterV1('z5363495@unsw.edu.au', 'aero123', 'Steve', 'Berrospi').authUserId;
+    user_id2 = authRegisterV1('z3329234@unsw.edu.au', 'aero321', 'Gary', 'Ang').authUserId;
+    user_id3 = authRegisterV1('z1319832@unsw.edu.au', 'aero456', 'Kenneth', 'Kuo').authUserId;
+    user_id4 = authRegisterV1('z4234824@unsw.edu.au', 'aero654', 'David', 'Pei').authUserId;
+  });
+  
+  afterEach(() => {
+    clearV1();
+  });
+  
+  test('Email doesn\'t belong to a user', () => {
+    expect(authLoginV1('z32132132@gmail.com', 'aero123')).toStrictEqual({error: 'error'});
+  });
+  
+  test('Password is not correct', () => {
+    expect(authLoginV1('z5363495@unsw.edu.au', 'aero321')).toStrictEqual({error: 'error'});
+  });
+  
+  test('Correct return type', () => {
+    expect(authLoginV1('z5363495@unsw.edu.au', 'aero123')).toStrictEqual(expect.objectContaining({
+      authUserId: user_id1,
+    },));
+  });
+});
+
