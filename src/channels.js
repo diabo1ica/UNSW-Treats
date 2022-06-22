84bf860d9fcf77773278b496a58725cfc895d610
Kenneth <kennethkuo25@outlook.com>
2022-06-22 22:44:57 +1000

    Finish ChannelCreate Implementation

--- src/channels.js
+++ src/channels.js
@@ -1,7 +1,39 @@
+import { getData } from './dataStore'
+
 function channelsCreateV1(authUserId, name, isPublic) {
+  if (name.length < 1 || name.length > 20) {
+    return {error: 'error'};
+  }
+  const data = getData();
+  const channels = channelsTemplate();
+  if (data.userIdCounter === 0) {
+    channels.channelId = 1;
+    data.channelIdCounter++;
+  }
+  else {
+    channels.channelId = data.channelIdCounter + 1;
+    data.channelIdCounter++;
+  }
+  
+  channels.name = name;
+  channels.isPublic = isPublic;
+  
+  for (let item of data.users) {
+    if (item.userId === authUserId) {
+      channels.members[name] = item.name;
+      channels.members.push({
+        uId: authUserId,
+        email: item.email,
+        nameFirst: item.firstname,
+        nameLast: item.lastname,
+        handleStr:'',
+      });
+    }
+  }
+  data.channels.push(channels);
   return {
-    channelId: 1,
-  };
+    channelId: channels.channelId,
+  }
 }
 
 function channelsListV1(authUserId) {
@@ -14,6 +46,18 @@ function channelsListallV1(authUserId) {
   return {
     channels: [] // see interface for contents
   };
+}
+
+function channelsTemplate() {
+  const channel = {
+    channelId:' ',
+    name: ' ',
+    isPublic: ' ',
+    members: [],  
+    messages: [], 
+  }
+  
+  return channel;
 }
 
 export { channelsCreateV1, channelsListV1, channelsListallV1 };
