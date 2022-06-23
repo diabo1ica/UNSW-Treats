c6ec435760b05021600725271727d6894ef39cee
David <z5361398@unsw.edu.au>
2022-06-23 16:07:42 +1000

    implementation for channelslist but test not working

--- src/other.js
+++ src/other.js
@@ -1,5 +1,14 @@
+import { getData } from './dataStore.js';
+
+// Clears the dataStore 
 function clearV1() {
+  let data = getData();
+  data.users = [];
+  data.channels = [];
+  data.userIdCounter = 0;
+  data.channelIdCounter = 0;
   return {};
 }
 
 export { clearV1 };
+
