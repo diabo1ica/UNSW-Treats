164c4e3690622ba3821942e41fd968b6538691b0
Kenneth <kennethkuo25@outlook.com>
2022-06-23 11:36:20 +1000

    Added ChannelInvite Tests

--- src/channel.test.js
+++ src/channel.test.js
@@ -150,5 +150,5 @@ test('Testing Invitation(do not exist (3))', () => {
   const c = channelsCreateV1(user_authUserId, 'Channel1', 'true');  
   const channel_id = c.channelId;
   
-  expect(channelInviteV1(u_id, channel_id, user_authUserId)).toStrictEqual({});
+  expect(channelInviteV1(u_id, channel_id, user_authUserId)).toStrictEqual({error: 'error'});
 });
