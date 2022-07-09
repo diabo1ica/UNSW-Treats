945c648c5a280400d81d31ba696fc40fa70f17fb
Gary <gary_ang.ang@student.unsw.edu.au>
2022-07-09 20:33:54 +1000

    Changed .js files to .ts

---
+++ src/dataStore.ts
@@ -0,0 +1,69 @@
+// YOU SHOULD MODIFY THIS OBJECT BELOW
+interface user {
+    nameFirst: string,
+    nameLast: string,
+    handleStr: string,
+    email: string,
+    password: string,
+    userId: number,
+    globalPermsId: number
+}
+
+interface member {
+  uId: number,
+  email: string,
+  nameFirst: string,
+  nameLast: string,
+  handleStr: string,
+  channelPermsId: number
+}
+
+interface channel {
+  channelId: number,
+  name: string,
+  isPublic: boolean,
+  members: member[],  
+  messages: string[], 
+}
+
+interface dataStr {
+  users: user[],
+  channels: channel[],
+  userIdCounter: number,
+  channelIdCounter: number
+};
+
+let data: dataStr = {
+  users: [],
+  channels: [],
+  userIdCounter: 0,
+  channelIdCounter: 0,
+};
+
+// YOU SHOULDNT NEED TO MODIFY THE FUNCTIONS BELOW IN ITERATION 1
+
+/*
+Example usage
+    let store = getData()
+    console.log(store) # Prints { 'names': ['Hayden', 'Tam', 'Rani', 'Giuliana', 'Rando'] }
+
+    names = store.names
+
+    names.pop()
+    names.push('Jake')
+
+    console.log(store) # Prints { 'names': ['Hayden', 'Tam', 'Rani', 'Giuliana', 'Jake'] }
+    setData(store)
+*/
+
+// Use get() to access the data
+function getData() {
+  return data;
+}
+
+// Use set(newData) to pass in the entire data object, with modifications made
+function setData(newData) {
+  data = newData;
+}
+
+export { getData, setData, user, member, channel, dataStr };
---
+++ src/other.ts
@@ -0,0 +1,14 @@
+import { getData, dataStr } from './dataStore';
+
+// Clears the dataStore 
+function clearV1() {
+  let data: dataStr = getData();
+  data.users = [];
+  data.channels = [];
+  data.userIdCounter = 0;
+  data.channelIdCounter = 0;
+  return {};
+}
+
+export { clearV1 };
+
