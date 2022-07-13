210b0b15a7807a43166776c57a2a3d15ea56a3de
Kenneth <kennethkuo25@outlook.com>
2022-07-13 22:22:46 +1000

    Added ChannelsCreate

--- src/server.ts
+++ src/server.ts
@@ -2,17 +2,18 @@ import express from 'express';
 import { echo } from './echo';
 import morgan from 'morgan';
 import config from './config.json';
-<<<<<<< HEAD
+
 import { channelsCreateV1 } from './channels';
-=======
-import { authRegisterV1 } from './auth.ts';
+import { authRegisterV1 } from './auth';
 import { channelDetailsV1 } from './channel';
 import { getData, setData, user, dataStr } from './dataStore';
 import { clearV1 } from './other';
->>>>>>> e8f4b6293d6e761f79ee7e113196b2b43be7bfc0
+import * as jose from 'jose';
+const decodeToken = (token: string): number => jose.UnsecuredJWT.decode(token).payload.uId as number;
 
 // Set up web app, use JSON
 const app = express();
+const generateToken = (uId: number):string => new jose.UnsecuredJWT({ uId: uId }).setIssuedAt(Date.now()).setIssuer(JSON.stringify(Date.now())).encode();
 app.use(express.json());
 
 const PORT: number = parseInt(process.env.PORT || config.port);
@@ -28,25 +29,26 @@ app.get('/echo', (req, res, next) => {
   }
 });
 
-<<<<<<< HEAD
+
 app.post('/channels/create/v2', (req, res) => {
-  const { token, name, isPublic } = req.body;
-  const channelId = channelsCreateV1(token, name, isPublic);
+  const { name, isPublic } = req.body;
+  const token = req.query.token as string;
+  const user_authUserId = decodeToken(token);
+  const channelId = channelsCreateV1(user_authUserId, name, isPublic);
   res.json({
     channelId: channelId,
   }); 
 });
 
-app.get('channels/list/v2', (
-=======
+
 app.post('/auth/register/v2', (req, res) => {
   const { email, password, nameFirst, nameLast } = req.body;
   const id = authRegisterV1(email, password, nameFirst, nameLast);
-  const data: dataStr = getData;
+  const data: dataStr = getData();
   let token = '';
   for (const user of data.users) {
-    if (id.authUserId === user.authUserId) {
-      token = generateToken();
+    if (id.authUserId === user.userId) {
+      token = generateToken(id.authUserId);
       user.tokenArray.push(token);
     }
   }
@@ -56,9 +58,10 @@ app.post('/auth/register/v2', (req, res) => {
   });
 });
 
+
 app.post('/auth/logout/v1', (req, res) => {
   const { token } = req.body;
-  const data: dataStr = getData;
+  const data: dataStr = getData();
   for (const user of data.users) {
     for (const i in user.tokenArray) {
       if (token === user.tokenArray[parseInt(i)]) {
@@ -69,10 +72,20 @@ app.post('/auth/logout/v1', (req, res) => {
   res.json({});
 });
 
+app.post('/channels/create/v2', (req, res) => {
+  const { token, name, isPublic } = req.body;
+  if ()
+  const channelId = channelsCreateV1(token, name, isPublic);
+  res.json({
+    channelId: channelId,
+  }); 
+});
+
+
 app.get('/channel/details/v2', (req, res) => {
   const token = req.query.token as string;
   const chId = parseInt(req.query.channelId as string);
-  const data: dataStr = getData;
+  const data: dataStr = getData();
   let userId = 0;
   for (const user of data.users) {
     if (user.some(obj => obj.tokenArray === token)) {
@@ -111,11 +124,11 @@ app.delete('/dm/remove/v1', (req, res) => {
 
 });
 
+
 app.delete('clear/v1', (req, res) => {
   clearV1();
   res.json({});
 });
->>>>>>> e8f4b6293d6e761f79ee7e113196b2b43be7bfc0
 
 // for logging errors
 app.use(morgan('dev'));
@@ -125,17 +138,3 @@ app.listen(PORT, HOST, () => {
   console.log(`⚡️ Server listening on port ${PORT} at ${HOST}`);
 });
 
-function generateToken() {
-  const token: number = Math.random() * Number.MAX_VALUE;
-  const tokenStr: string = token.toString();
-  const data: dataStr = getData();
-  for (const user of data.users) {
-    // Loop to find duplicate
-    for (const userToken of user.tokenArray) {
-      if (tokenStr === userToken) {
-        return generateToken(); // Recursion
-      }
-    }
-  }
-  return tokenStr;
-}
