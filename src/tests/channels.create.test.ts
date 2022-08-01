4acde8d1648f2e50a62af90d353a26d575403df9
Kenneth <kennethkuo25@outlook.com>
2022-08-02 00:19:26 +1000

    edited

--- src/tests/channels.create.test.ts
+++ src/tests/channels.create.test.ts
@@ -1,42 +1,21 @@
 import { requestRegister, requestLogin, requestChannelsCreate, requestClear } from './request';
 import { OK, INPUT_ERROR } from './request';
 
-test('Testing ChannelsCreate (error)', () => {
-  requestClear();
-  requestRegister('justinbieber@gmail.com', '1122334455', 'Justin', 'Bieber');
-  const a = requestLogin('justinbieber@gmail.com', '1122334455');
-  const authUserId = a.body.authUserId;
-  expect(requestChannelsCreate(authUserId, '', true).statusCode).toStrictEqual({INPUT_ERROR});
-});
+describe('channels path tests', () => {
+  let userID : string;
+  let token;
+  beforeEach(() => {
+    requestClear();
+    userID = requestRegister('Alalalyeehoo@gmail.com', 'Sk8terboiyo', 'Jingisu', 'Kan').body.token;
+  });
+  test('ChannelsCreate Successfull', () => {
+    expect(requestChannelsCreate(userID, 'Channel1', true).statusCode).toStrictEqual(OK);
+    expect(requestChannelsCreate(userID, 'Channel1', true).body).toStrictEqual(expect.objectContaining({
+      channelId: expect.any(Number),
+    }));
+  });
 
-test('Testing ChannelsCreate (error)', () => {
-  requestClear();
-  requestRegister('justinbieber@gmail.com', '1122334455', 'Justin', 'Bieber');
-  const a = requestLogin('justinbieber@gmail.com', '1122334455');
-  const authUserId = a.body.authUserId;
-  expect(requestChannelsCreate(authUserId, 'IamcurrrentlystudyingcomputerscienceinUNSW', true).statusCode).toStrictEqual({INPUT_ERROR});
-});
-
-test('Testing ChannelsCreate (no error)', () => {
-  requestClear();
-  requestRegister('justinbieber@gmail.com', '1122334455', 'Justin', 'Bieber');
-  const a = requestLogin('justinbieber@gmail.com', '1122334455');
-  const authUserId = a.body.authUserId;
-  const b = requestChannelsCreate(authUserId, 'Channel1', true);
-  expect(b.statusCode).toStrictEqual(OK);
-  expect(b.body).toStrictEqual(expect.objectContaining({
-    channelId: expect.any(Number),
-  }));
-});
-
-test('Testing ChannelsCreate (no error)', () => {
-  requestClear();
-  requestRegister('justinbieber@gmail.com', '1122334455', 'Justin', 'Bieber');
-  const a = requestLogin('justinbieber@gmail.com', '1122334455');
-  const authUserId = a.body.authUserId;
-  const b = requestChannelsCreate(authUserId, 'Channel2', false);
-  expect(b.statusCode).toStrictEqual(OK);
-  expect(b.body).toStrictEqual(expect.objectContaining({
-    channelId: expect.any(Number),
-  }));
-});
+  test('ChannelsCreate Unsuccessfull', () => {
+    expect(requestChannelsCreate(userID, 'Iloveyoubabyandwillmarryyouchannel', true).statusCode).toStrictEqual({INPUT_ERROR});
+  });
+  });
