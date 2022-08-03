3f8cb7e7bf17f3dbc818445ab26b439de2363e3a
Kenneth <kennethkuo25@outlook.com>
2022-08-04 01:53:21 +1000

    Added UserStats Tests

---
+++ src/tests/user.stats.test.ts
@@ -0,0 +1,47 @@
+import { requestUserStats, requestChannelsCreate, requestRegister, requestLogin, requestClear, INPUT_ERROR } from './request';
+
+describe('search/v1 tests', () => {
+    let token : string;
+    let userID : number;
+    let userID2 : number;
+    let channelID : number;
+    let uIds: number[];
+    let dmId: number;
+    let mId: number;
+    beforeEach(() => {
+      requestClear();
+      requestRegister('Alalalyeehoo@gmail.com', 'Sk8terboiyo', 'Jingisu', 'Kan');
+      const obj = requestLogin('Alalalyeehoo@gmail.com', 'Sk8terboiyo');
+      token = obj.body.token;
+      userID = obj.body.authUserId;
+      channelID = requestChannelsCreate(token, 'Channel1', true).body.channelId;
+    });
+
+    test('user/stats/v1 Successfull', () => {
+      expect(requestUserStats(token).body).toStrictEqual({
+        channelsJoined: expect.arrayContaining([
+          {
+            numChannelsJoined: 0,
+            timeStamp: expect.any(Number),
+          }
+        ]),
+        dmsJoined: [],
+        messagesSent:[],
+        involvementRate: 1,
+      });
+    });
+
+    test('user/stats/v1 Empty', () => {
+      requestClear();
+      requestRegister('Alalalyeehoo@gmail.com', 'Sk8terboiyo', 'Jingisu', 'Kan');
+      const obj = requestLogin('Alalalyeehoo@gmail.com', 'Sk8terboiyo');
+      token = obj.body.token;
+      expect(requestUserStats(token).body).toStrictEqual({
+        channelsJoined: [],
+        dmsJoined: [],
+        messagesSent:[],
+        involvementRate: 0,
+      });
+    });
+    
+});
