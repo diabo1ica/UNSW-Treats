2f6ab9fd0d288c5d29271aaaca4c91bbbb936efb
Kenneth <kennethkuo25@outlook.com>
2022-08-03 16:52:43 +1000

    New DataStore

--- src/dataStore.ts
+++ src/dataStore.ts
@@ -1,5 +1,8 @@
 import fs from 'fs';
-interface User {
+export const THUMBSUP = 1;
+export const VALIDREACTS = [THUMBSUP];
+
+export interface User {
   nameFirst: string,
   nameLast: string,
   handleStr: string,
@@ -9,43 +12,58 @@ interface User {
   globalPermsId: number,
 }
 
-interface Member {
+export interface Member {
   uId: number,
   channelPermsId: number
 }
 
-interface Message {
+export interface React {
+  reactId: number,
+  uIds: number[],
+  isThisUserReacted?: boolean
+}
+
+export interface Message {
   messageId: number,
   uId: number,
   message: string,
   timeSent: number,
+  isPinned: boolean,
+  reacts: React[],
+  dmId: number,
+  channelId: number,
 }
 
-interface Channel {
+export interface StandUp {
+  timeFinish: number,
+  messageId: number,
+}
+
+export interface Channel {
   channelId: number,
   name: string,
   isPublic: boolean,
   members: Member[],
-  messages: Message[],
+  standUp: StandUp,
 }
 
-interface DmMember {
+export interface DmMember {
   uId: number,
   dmPermsId: number,
 }
 
-interface Dm {
+export interface Dm {
   members: DmMember[],
-  messages: Message[],
   dmId: number,
   creatorId: number,
   name: string
 }
 
-interface DataStr {
+export interface DataStr {
   users: User[],
   channels: Channel[],
   dms: Dm[],
+  messages: Message[],
   tokenArray: string[],
   userIdCounter: number,
   channelIdCounter: number,
@@ -57,6 +75,7 @@ let data: DataStr = {
   users: [],
   channels: [],
   dms: [],
+  messages: [],
   tokenArray: [],
   userIdCounter: 0,
   channelIdCounter: 0,
@@ -81,22 +100,21 @@ Example usage
 */
 
 // Use get() to access the data
-function getData(load = false, name = './data.json') {
+export function getData(load = false, name = 'data.json') {
   if (load === true && fs.existsSync(name)) {
-    const loadedData = JSON.parse(fs.readFileSync(name, { encoding: 'utf8' }));
+    const loadedData = JSON.parse(fs.readFileSync('./' + name, { encoding: 'utf8' }));
     data.users = loadedData.users;
     data.channels = loadedData.channels;
     data.dms = loadedData.dms;
-    console.log('\'data.json\' successfully loaded');
+    console.log(`'${name}' successfully loaded`);
   }
   return data;
 }
 
 // Use set(newData) to pass in the entire data object, with modifications made
-function setData(newData: DataStr, name = './data.json') {
+export function setData(newData: DataStr, name = './data.json') {
   fs.writeFileSync(name, JSON.stringify(newData, null, 4));
   data = newData;
 }
 
-export { getData, setData, User, Member, Channel, DataStr, Dm, Message, DmMember };
 
