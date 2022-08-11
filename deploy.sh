d1fc645740bc9951be1ad29b9a0d9a51bb53dfea
cs1531 <cs1531@cse.unsw.edu.au>
2022-08-11 21:16:21 +1000

    Results push

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-WORKING_DIRECTORY="~/www/cs1531deploy"
-
-USERNAME="t14aaero"
-SSH_HOST="ssh-t14aaero.alwaysdata.net"
-
-scp -r ./package.json ./package-lock.json ./tsconfig.json ./src "$USERNAME@$SSH_HOST:$WORKING_DIRECTORY"
-ssh "$USERNAME@$SSH_HOST" "cd $WORKING_DIRECTORY && npm install --only=production"
--- src/tests/sleep.ts
+++
@@ -1,7 +0,0 @@
-export function sleepFor(sleepDuration: number) {
-  const now = new Date().getTime();
-  let time = new Date().getTime();
-  while (time < now + sleepDuration) {
-    time = new Date().getTime();
-  }
-}
